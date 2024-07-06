1206be36fb26d666e76106ca05be3bf1212dcad1
JALZ <jannus.largo@correounivalle.edu.co>
2024-07-06 15:02:45 -0500

    replace name server for index

---
+++ index.js
@@ -0,0 +1,30 @@
+// Import database and dotenv
+import "dotenv/config.js";
+import "./config/connectionDb.js";
+
+// Import modules
+import express from 'express';
+import cors from "cors";
+import morgan from "morgan";
+
+// Import routes
+import indexRouter from "./routes/index.js";
+
+// Import error handlers
+import not_found_handler from "./middlewares/not_found_handler.js";
+import error_handler from "./middlewares/error_handler.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(morgan("dev"));
+
+app.use("/api", (req, res, next) => { res.send(`[ROUTER]: Loading main route...`); return next() }, indexRouter)
+app.use(not_found_handler)
+app.use(error_handler);
+
+app.listen(process.env.PORT, () => console.log('Server Ready in port:', process.env.PORT));
+
+export default app;
